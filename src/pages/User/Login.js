f37751a4a5d50e356fae9836389cc35a98ad8965
remotesc2 <remotesc2@foxmail.com>
2018-07-06 17:12:21 +0800

    [refactor] userName --> loginName

--- src/pages/User/Login.js
+++ src/pages/User/Login.js
@@ -55,7 +55,7 @@ export default class LoginPage extends Component {
               login.type === 'account' &&
               !login.submitting &&
               this.renderMessage('账户或密码错误（admin/888888）')}
-            <UserName name="userName" placeholder="admin/user" />
+            <UserName name="loginName" placeholder="admin/user" />
             <Password name="password" placeholder="888888/123456" />
           </Tab>
           <Tab key="mobile" tab="手机号登录">
