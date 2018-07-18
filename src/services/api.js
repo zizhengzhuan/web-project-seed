8153beba47e9b1671eb6daf6ce22f4ff293108b0
remotesc2 <remotesc2@foxmail.com>
2018-07-18 18:58:16 +0800

    [feat] init.json 中支持配置 oms 版本

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,43 @@
 import { http } from 'yc';
 import request from '../utils/request';
-import { getSys } from '../utils/sysConfig';
+import { getSys, getInit } from '../utils/sysConfig';
+import { getItem } from '../utils/authority';
 
 const { get } = http;
 const sys = getSys();
+const init = getInit();
+const username = getItem('username');
 
-export async function accountLogin(params) {
+/**
+ * 针对 oms 2.1 版本的登陆服务
+ * @param params
+ * @return {Promise<void>}
+ */
+async function loginV2(params) {
+  const { password, userName } = params;
+  const res = await get({
+    svn: 'OMS_SVR',
+    path: 'login',
+    data: { password, username: userName, sys },
+  });
+  res.data = {
+    token: 'IAmToken',
+  };
+  return res;
+}
+
+/**
+ * 针对 oms 2.1 版本的登出服务
+ * @param params
+ * @return {Promise<void>}
+ */
+async function logoutV2() {
+  return new Promise(resolve => {
+    resolve('success');
+  });
+}
+
+async function loginV3(params) {
   const { password, loginName } = params;
   return get({
     svn: 'OMS_SVR',
@@ -14,7 +46,7 @@ export async function accountLogin(params) {
   });
 }
 
-export async function accountLogout() {
+async function logoutV3() {
   return get({
     svn: 'OMS_SVR',
     path: 'user/logout',
@@ -22,6 +54,56 @@ export async function accountLogout() {
   });
 }
 
+/**
+ * 针对 oms 2.1 版本的登出服务
+ * @param params
+ * @return {Promise<void>}
+ */
+async function getUserInfoV2() {
+  const res = await get({
+    svn: 'OMS_SVR',
+    path: 'getUserInfo',
+    data: { username, sys },
+  });
+  if (res) {
+    return {
+      data: {
+        user: res.user,
+      },
+    };
+  }
+  return null;
+}
+
+async function getUserInfoV3() {
+  return get({
+    svn: 'OMS_SVR',
+    path: 'user/getUserInfoByToken',
+    data: { sys },
+  });
+}
+
+export async function accountLogin(params) {
+  if (init.oms && init.oms === '2') {
+    return loginV2(params);
+  }
+  return loginV3(params);
+}
+
+export async function accountLogout() {
+  if (init.oms && init.oms === '2') {
+    return logoutV2();
+  }
+  return logoutV3();
+}
+
 export async function queryNotices() {
   return request('/api/notices');
+}
+
+export async function queryCurrent() {
+  if (init.oms && init.oms === '2') {
+    return getUserInfoV2();
+  }
+  return getUserInfoV3();
 }
--- src/utils/authority.js
+++ src/utils/authority.js
@@ -5,6 +5,7 @@ const { sys } = iniConfig;
 
 const authorityNameSpace = `${sys}-authority`;
 const tokenNameSpace = `${sys}-token`;
+const storeNameSpace = `${sys}-store`;
 // use localStorage to store the authority info, which might be sent from server in actual project.
 export function getAuthority() {
   return localStorage.getItem(authorityNameSpace) || 'admin';
@@ -20,4 +21,35 @@ export function getToken() {
 
 export function setToken(token = '') {
   return localStorage.setItem(tokenNameSpace, token);
+}
+
+export function setItem(key, value) {
+  const str = localStorage.getItem(storeNameSpace) || '';
+  let store = {};
+  try {
+    if (str.length > 0) {
+      store = JSON.parse(str);
+    }
+  } catch (e) {
+    console.error(e);
+    clearItem();
+  }
+  store[key] = value;
+  localStorage.setItem(storeNameSpace, JSON.stringify(store));
+}
+
+export function getItem(key) {
+  const str = localStorage.getItem(storeNameSpace) || '';
+  try {
+    if (str.length > 0) {
+      return JSON.parse(str)[key] || '';
+    }
+  } catch (e) {
+    console.error(e);
+  }
+  return '';
+}
+
+export function clearItem() {
+  localStorage.setItem(storeNameSpace, '');
 }
