71deb40b86ad9d99ae11ca6795cb292004f5a1a5
remotesc2 <remotesc2@foxmail.com>
2018-07-27 10:02:03 +0800

    [feat] 针对 oms 2.0 进行相关调整

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -1,6 +1,6 @@
 import { getNotices } from './mock/notices';
 import { format, delay } from 'roadhog-api-doc';
-import { login, logout, getUserInfoByToken } from './mock/login';
+import { login, logout, loginV2, getUserInfoByToken, getUserInfo } from './mock/login';
 
 // 是否禁用代理
 const noProxy = process.env.NO_PROXY === 'true';
@@ -8,10 +8,12 @@ const apiurl = 'http://192.168.1.204:8800';
 
 // 代码中会兼容本地 service mock 以及部署站点的静态数据
 const proxy = {
-  // 支持值为 Object 和 Array
   'GET /api/CityServer/rest/omsService/user/login': login,
   'GET /api/CityServer/rest/omsService/user/logout': logout,
   'GET /api/CityServer/rest/omsService/user/getUserInfoByToken': getUserInfoByToken,
+  'GET /api/ServiceEngine/rest/userService/login': loginV2,
+  'GET /api/ServiceEngine/rest/userService/getUserInfo': getUserInfo,
+
   'GET /api/notices': getNotices,
 };
 
