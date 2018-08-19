4797cae4fd8ceb041a3ebef32793aad2595a84f0
remotesc2 <remotesc2@foxmail.com>
2018-08-19 16:44:41 +0800

    [refactor] 精简配置

--- config/bee.config.js
+++ config/bee.config.js
@@ -1,12 +1,5 @@
 module.exports = {
-  layouts: {
-    layoutA: '1.0.0',
-  },
-  pages: {
-    bpmn: '1.0.0',
-    form: '1.0.0',
-    oms: '1.0.0',
-  },
-  scan: ['layoutB', 'UserB'],
-  registry: 'http://192.168.8.254:8081/repository/npm-private/',
+  scan: ['layout-layout-b', 'page-user-b'],
+  dash2Camel: true,
+  registry: 'http://192.168.31.231:4873/',
 };
