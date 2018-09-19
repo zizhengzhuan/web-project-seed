d38926a1a5bc4d5508997620e4488ded646f645b
remotesc2 <remotesc2@foxmail.com>
2018-09-19 13:48:07 +0800

    [chore] 调整为依赖布局

--- config/bee.config.js
+++ config/bee.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  scan: ['layout-layout-b', 'page-user-b'],
+  scan: ['layout-layout-a', 'page-user-a'],
   dash2Camel: true,
   registry: 'http://192.168.8.254:8081/repository/npm-private/',
 };
